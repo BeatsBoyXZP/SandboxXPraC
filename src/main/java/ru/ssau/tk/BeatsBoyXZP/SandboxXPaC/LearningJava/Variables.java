099afaab2a3d0bd44512c37ef303f892f62fedc1
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-10-11 14:50:01 +0300

    Создан класс Variables, в нём описана часть примитивных типов.

--- src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/LearningJava/Variables.java
+++ src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/LearningJava/Variables.java
@@ -1,4 +1,21 @@
 package ru.ssau.tk.BeatsBoyXZP.SandboxXPaC.LearningJava;
 
 public class Variables {
+    public static void main (String [] args) {
+        int myInt;
+        myInt = 13;
+        short myShort;
+        myShort = 7828;
+        long saree;
+        saree = 2123;
+        char c ='f';
+        boolean ste;
+        ste = false;//false or true
+        float ris;
+        ris = 35.2f;
+        byte sik;
+        sik = 110; //{-128...127}
+
+        System.out.println(myInt);
+    }
 }
