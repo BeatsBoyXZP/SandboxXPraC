909f7ad766b473ed49fced7cf606156084846a29
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-10-12 23:54:00 +0300

    Цикл While.

--- src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Person.java
+++ src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Person.java
@@ -3,6 +3,29 @@ package ru.ssau.tk.BeatsBoyXZP.SandboxXPaC.TasksDataTypes;
 public class Person {
     private String firstName;
     private String lastName;
-    private double passportId;
+    private int passportId;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
 }
