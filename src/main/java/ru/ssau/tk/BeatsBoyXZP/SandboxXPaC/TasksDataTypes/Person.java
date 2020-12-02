e3cddc8674bb661f789c9cefb2f7061077eb3861
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-12-02 05:22:30 +0300

    Сделал задание 1 и 2.

--- src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Person.java
+++ src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Person.java
@@ -5,6 +5,24 @@ public class Person {
     private String lastName;
     private int passportId;
 
+    public Person(){
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int passportId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+    }
+
+    public Person(int passportId) {
+        this.passportId = passportId;
+    }
+
     public String getFirstName() {
         return firstName;
     }
