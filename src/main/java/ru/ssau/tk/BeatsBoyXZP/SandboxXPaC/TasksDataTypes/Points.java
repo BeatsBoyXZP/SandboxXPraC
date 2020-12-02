583aa2952aad8ea429647ecf941bd150c4ba2017
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2020-12-02 12:31:02 +0400

    Сделал задание 5.

--- src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Points.java
+++ src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Points.java
@@ -19,4 +19,8 @@ public class Points{
     public static Point divide(Point a1, Point a2){
         return new Point(a1.x / a2.x, a1.y / a2.y, a1.z / a2.z );
     }
+
+    public static Point enlarge(Point b, double w){
+        return new Point(b.x * w, b.y * 2, b.z * w);
+    }
 }
