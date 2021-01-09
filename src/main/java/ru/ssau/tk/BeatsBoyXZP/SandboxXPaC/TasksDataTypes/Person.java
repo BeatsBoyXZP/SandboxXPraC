41cd7f4c876a868dd408bd8949d83734bb82fbc8
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2021-01-09 20:20:47 +0400

    1.11

--- src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Person.java
+++ src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Person.java
@@ -5,12 +5,16 @@ public class Person {
     private String lastName;
     private int passportId;
 
-    public Person(){
+    public Person() {
     }
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Person(int passportId) {
+        this.passportId = passportId;
     }
 
     public Person(String firstName, String lastName, int passportId) {
@@ -19,31 +23,34 @@ public class Person {
         this.passportId = passportId;
     }
 
-    public Person(int passportId) {
-        this.passportId = passportId;
-    }
-
     public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        return firstName = firstName;
     }
 
     public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+        return lastName = lastName;
     }
 
     public int getPassportId() {
-        return passportId;
+        return passportId = passportId;
     }
 
-    public void setPassportId(int passportId) {
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    void setPassportId(int passportId) {
         this.passportId = passportId;
     }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+
 }
