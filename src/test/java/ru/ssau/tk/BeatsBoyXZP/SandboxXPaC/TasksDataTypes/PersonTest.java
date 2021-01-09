46f8a2abebeacbaf2d763dc2a115debbd22e6eaf
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2021-01-09 05:58:27 +0400

    1.10

--- src/test/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/PersonTest.java
+++ src/test/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/PersonTest.java
@@ -5,7 +5,19 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class PersonTest {
-    @Test
+        Person person1 = new Person();
+
+        @Test
+        public void testGetAndSet() {
+            person1.setFirstName("Key");
+            person1.setLastName("Black");
+            person1.setPassportId(15163);
+            assertEquals(person1.getFirstName(), "Key");
+            assertEquals(person1.getLastName(), "Black");
+            assertEquals(person1.getPassportId(), 15163, 0.000000001);
+        }
+
+        @Test
     public void testTestToString() {
         Person human = new Person("Alexander", "Petrov", 3124876);
         Person unknown = new Person("Dom", "Kepler", 3212893);
