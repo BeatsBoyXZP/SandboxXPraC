34e6e4cbd5cd2c5e3bb68f5ba06d60ac560366e4
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2021-01-13 03:09:46 +0400

    2.1

---
+++ src/main/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Massif.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.BeatsBoyXZP.SandboxXPaC.TasksDataTypes;
+
+public class Massif {
+    public int[] massifSize(int size) {
+        int[] massif1 = new int[size];
+        return massif1;
+    }
+}
