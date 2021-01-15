bab22590faee6f14e9bfbe8a68288c75f0c08d0a
BeatsBoyXZP <bgv-gleb01@yandex.ru>
2021-01-15 04:10:47 +0400

    3.17

--- src/test/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Massif2_31Test.java
+++ src/test/java/ru/ssau/tk/BeatsBoyXZP/SandboxXPaC/TasksDataTypes/Massif2_31Test.java
@@ -1,4 +1,19 @@
 package ru.ssau.tk.BeatsBoyXZP.SandboxXPaC.TasksDataTypes;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class Massif2_31Test {
+
+    @Test
+    public void testTestToString() {
+        Massif2_31 firstMatrix = new Massif2_31(2, 2);
+        firstMatrix.setAt(0, 0, 1);
+        firstMatrix.setAt(0, 1, 2);
+        firstMatrix.setAt(1, 0, 3);
+        firstMatrix.setAt(1, 1, 4);
+        System.out.println(firstMatrix);
+
+    }
 }
